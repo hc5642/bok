7dd86141ceffb95b6ef20b8f4f3b0118403667c6
2310449 <hyonchul.oh@bok.or.kr>
2023-10-01 08:32:13 +0900

    bokworks-kafka-sample project

---
+++ bokworks-kafka-sample/src/main/java/com/bok/ohc/kfk/BokworksKafkaSampleApplication.java
@@ -0,0 +1,13 @@
+package com.bok.ohc.kfk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokworksKafkaSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BokworksKafkaSampleApplication.class, args);
+	}
+
+}
---
+++ bokworks-kafka-sample/src/test/java/com/bok/ohc/kfk/BokworksKafkaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bok.ohc.kfk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokworksKafkaSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
